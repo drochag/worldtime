505b7e415db1ba2f6454c6070f0ea37598994f77
Dan Rocha <drochag1@gmail.com>
2020-06-20 18:40:34 -0500

    Adding prettier & tslint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="max-w-4xl mx-auto flex flex-col">
-      <h1 className="text-6xl text-white leading-tight mt-12 text-center">
-        WorldtimeLite
-      </h1>
+      <h1 className="text-6xl text-white leading-tight mt-12 text-center">WorldtimeLite</h1>
       <div className="w-full p-3 bg-gray-100 mt-10 rounded-lg shadow-xl">
-        <p className="text-base text-gray-700 leading-normal">
-          WorldtimeLite content
-        </p>
+        <p className="text-base text-gray-700 leading-normal">WorldtimeLite content</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
