7f6fd97aa5e0d4fc184e4daacb5571cf5f45cd78
Dan Rocha <drochag1@gmail.com>
2020-06-20 18:29:53 -0500

    Introducing tailwind

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,20 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="max-w-4xl mx-auto flex flex-col">
+      <h1 className="text-6xl text-white leading-tight mt-12 text-center">
+        WorldtimeLite
+      </h1>
+      <div className="w-full p-3 bg-gray-100 mt-10 rounded-lg shadow-xl">
+        <p className="text-base text-gray-700 leading-normal">
+          WorldtimeLite content
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import './tailwind.generated.css';
 
 ReactDOM.render(
   <React.StrictMode>
