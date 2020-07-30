4d89ec6cb73d6af62c8fb1be10b8f691a933dadf
Dan Rocha <drochag1@gmail.com>
2020-07-30 01:15:43 -0500

    footer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
 import Search from 'components/Search'
 import Suggestions from 'components/Suggestions'
 
@@ -12,12 +15,27 @@ const App: React.FC = () => {
     return () => clearInterval(interval)
   }, [])
   return (
-    <div className="max-w-7xl mx-auto flex flex-col">
-      <h1 className="text-4xl md:text-6xl text-white leading-tight mt-12 text-center">
-        WorldtimeLite
-      </h1>
-      <Suggestions time={time}>{props => <Search {...props} />}</Suggestions>
-    </div>
+    <>
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl md:text-6xl text-white leading-tight mt-12 text-center">
+          WorldtimeLite
+        </h1>
+      </div>
+      <div className="max-w-7xl w-full mx-auto flex-grow">
+        <Suggestions time={time}>{props => <Search {...props} />}</Suggestions>
+      </div>
+      <footer className="text-white pb-4 mt-4 text-center w-full">
+        Made with <FontAwesomeIcon icon={faHeart} /> by&nbsp;
+        <a
+          className="underline"
+          href="http://danielrocha.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dan Rocha
+        </a>
+      </footer>
+    </>
   )
 }
 
