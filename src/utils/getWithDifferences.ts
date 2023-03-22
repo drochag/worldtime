6c435dc70689d22d63477c63d7bc43f438441481
t-fernando <tfernandogomezbonilla@gmail.com>
2023-03-22 10:58:05 -0600

    Math refactor

--- src/utils/getWithDifferences.ts
+++ src/utils/getWithDifferences.ts
@@ -5,17 +5,13 @@ export default function getWithDifferences(
   time: Date
 ): ExtendedSuggestionWithDifference[] {
   const withDifference = suggestions.map((s, index) => {
-    const rawDifference = (s.timezone.rawOffset - (suggestions[0].timezone.rawOffset ?? s.timezone.rawOffset)) /
-    60 /
-    60
-    const dstDifference = (s.timezone.dstOffset - (suggestions[0].timezone.dstOffset ?? s.timezone.dstOffset)) /
-    60 /
-    60
+    const rawDifference = (s.timezone.rawOffset - (suggestions[0].timezone.rawOffset ?? s.timezone.rawOffset))
+    const dstDifference = (s.timezone.dstOffset - (suggestions[0].timezone.dstOffset ?? s.timezone.dstOffset))
     return({
       ...s,
       recentlyAdded: index === suggestions.length,
       index,
-      difference:rawDifference+dstDifference,
+      difference:(rawDifference+dstDifference)/60/60,
       time: new Date(
         time
           .toLocaleString('en-us', {
