96be6901ce76cb8deb1063905de1694ad6f48266
t-fernando <tfernandogomezbonilla@gmail.com>
2023-03-22 10:25:30 -0600

    Added daylight offset to difference calculation

--- src/utils/getWithDifferences.ts
+++ src/utils/getWithDifferences.ts
@@ -4,28 +4,32 @@ export default function getWithDifferences(
   suggestions: ExtendedSuggestion[],
   time: Date
 ): ExtendedSuggestionWithDifference[] {
-  const withDifference = suggestions.map((s, index) => ({
-    ...s,
-    recentlyAdded: index === suggestions.length,
-    index,
-    difference:
-      (s.timezone.rawOffset - (suggestions[0].timezone.rawOffset ?? s.timezone.rawOffset)) /
-      60 /
-      60,
-    time: new Date(
-      time
-        .toLocaleString('en-us', {
-          timeZone: s.timezone.timeZoneId,
-          hour12: false,
-          weekday: 'long',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-        })
-        .replace('24:', '00:').replace(' at ', ' ')
-    ),
-  }))
+  const withDifference = suggestions.map((s, index) => {
+    const rawDifference = (s.timezone.rawOffset - (suggestions[0].timezone.rawOffset ?? s.timezone.rawOffset)) /
+    60 /
+    60
+    const dstDifference = (s.timezone.dstOffset - (suggestions[0].timezone.dstOffset ?? s.timezone.dstOffset)) /
+    60 /
+    60
+    return({
+      ...s,
+      recentlyAdded: index === suggestions.length,
+      index,
+      difference:rawDifference+dstDifference,
+      time: new Date(
+        time
+          .toLocaleString('en-us', {
+            timeZone: s.timezone.timeZoneId,
+            hour12: false,
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })
+          .replace('24:', '00:').replace(' at ', ' ')
+      ),
+    })})
   return withDifference
 }
